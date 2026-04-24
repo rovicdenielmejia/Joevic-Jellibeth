940b2edcab584d9252c0230ad1ae0fee5d700e93
Rovic <rovicdenielmejia@gmail.com>
2026-04-24 21:31:54 +0800

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useEffect } from 'react'
+import { useState, useMemo, useEffect, useRef } from 'react'
 import { Routes, Route, useSearchParams, useParams, useNavigate } from 'react-router-dom'
 import Header from './components/Header'
 import Hero from './components/Hero'
